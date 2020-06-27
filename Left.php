a8945ce173c4cf564950224a0a1c428970e6be88
tahanialsh <Tahaninfc1@outlook.com>
2020-06-27 20:36:20 +0300

    Add all

---
+++ Left.php
@@ -0,0 +1,15 @@
+<?php
+
+$user = "root";
+$password = "";
+$db = 'dbcontrol';
+
+$conn=new mysqli("localhost",$user,$password,$db) or die ("unable to connect");
+//echo"test";
+?>
+
+<html>
+<body style="background:<?php echo '#3498db';?>">
+<h2 style="color:<?php echo'montserrat';?>">L</h2>
+</body>
+</html>
